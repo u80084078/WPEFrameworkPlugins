6b8171cd4cc5f7cf1ef4c55f427fc168680dafc1
Sander van der Maar <sandervandermaar@gmail.com>
2018-10-22 10:48:21 +0200

    [OpenCDMi] Hardcodes playready for now

--- OpenCDMi/FrameworkRPC.cpp
+++ OpenCDMi/FrameworkRPC.cpp
@@ -600,7 +600,7 @@ namespace Plugin {
                 ASSERT (parent != nullptr);
 
                 // TODO: figure out if we can know/need to know selected key system here
-                _systemExt = dynamic_cast<CDMi::IMediaKeysExt*>(_parent.KeySystem("com.metrological.null"));
+                _systemExt = dynamic_cast<CDMi::IMediaKeysExt*>(_parent.KeySystem("com.microsoft.playready"));
             }
             virtual ~AccessorOCDM() {
                 TRACE_L1("Released the AccessorOCDM server side [%d]", __LINE__);
@@ -761,7 +761,7 @@ namespace Plugin {
             {
 
                 // TODO: key system
-                const string keySystem = "com.metrological.null";
+                const string keySystem = "com.microsoft.playready";
                 CDMi::IMediaKeysExt* system = dynamic_cast<CDMi::IMediaKeysExt*>(_parent.KeySystem(keySystem));
 
                 if (system == nullptr) {
